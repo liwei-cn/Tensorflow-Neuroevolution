33c88d3dc25b048935d6806f0dd70bbe4592aee6
Paul Pauls <mail@paulpauls.de>
2020-03-23 21:13:04 +0100

    Remove build directory from readthedocs and add custom conf.py option for readthedocs compatibility

--- documentation/readthedocs/source/conf.py
+++ documentation/readthedocs/source/conf.py
@@ -53,3 +53,10 @@ html_theme = 'alabaster'
 # relative to this directory. They are copied after the builtin static files,
 # so a file named "default.css" will overwrite the builtin "default.css".
 html_static_path = ['_static']
+
+
+# -- Custom User options -------------------------------------------------------
+
+# My option to make a Sphinx skeleton work for readthedocs.org
+master_doc = 'index'
+
