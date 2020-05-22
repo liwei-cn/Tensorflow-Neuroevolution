8dc70d373838fdc1df15487bfeef1abd4eead4da
Paul Pauls <mail@paulpauls.de>
2020-05-22 17:09:23 +0200

    Remove randomizing of fitness results and actually apply genomes to environment; Remove Bug Workaround warning for random NaNs cause by TF

--- tfne/environments/other/xor_environment.py
+++ tfne/environments/other/xor_environment.py
@@ -46,23 +46,12 @@ class XOREnvironment(BaseEnvironment):
             raise RuntimeError("Genome to evaluate ({}) does not supply an optimizer and no standard optimizer defined"
                                "for XOR environment as of yet.")
 
-        '''
         # Compile and train model
         model.compile(optimizer=optimizer, loss=self.loss_function)
         model.fit(x=self.x, y=self.y, epochs=self.epochs, batch_size=self.batch_size, verbose=self.verbosity)
 
         # Evaluate and return its fitness
         evaluated_fitness = float(100 * (1 - self.loss_function(self.y, model.predict(self.x))))
-
-        # FIXME BUG WORKAROUND
-        # Sometimes loss function randomly returns NaN. Problem confirmed by Tensorflow.
-        # See bug report: https://github.com/tensorflow/tensorflow/issues/38457
-        if tf.math.is_nan(evaluated_fitness):
-            evaluated_fitness = 0.0
-        '''
-        # FIXME temporary for quick testing of dev version
-        import random
-        evaluated_fitness = random.uniform(0, 100)
 
         return round(evaluated_fitness, 4)
 
